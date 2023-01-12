a87118903af3f956f6c5ffe631a6df7329675625
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2023-01-12 19:12:03 -0300

    first commit

---
+++ src/app/upload.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Upload, upload } from './upload';
+
+@Injectable({ providedIn: 'root' })
+export class UploadService {
+    constructor(private http: HttpClient) { }
+
+    upload(file: File): Observable<Upload> {
+        const data = new FormData();
+        data.append('file', file);
+        return this.http
+            .post('http://localhost:8080/agentes', data, {
+                reportProgress: true,
+                observe: 'events'
+            })
+            .pipe(upload());
+    }
+}
